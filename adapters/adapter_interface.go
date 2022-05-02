472d62852ea418863bd8cea9fc1811ae05507f43
Anbraten <anton@ju60.de>
2022-05-02 20:18:48 +0200

    undo change

--- adapters/adapter_interface.go
+++ adapters/adapter_interface.go
@@ -7,7 +7,7 @@ type DatabaseAdapter interface {
 	CreateDatabase(ctx context.Context, database string) error
 	DeleteDatabase(ctx context.Context, database string) error
 	HasDatabaseUserWithAccess(ctx context.Context, database string, username string) (bool, error)
-	CreateDatabaseUser(ctx context.Context, database string, username string, quotedPassword string) error
+	CreateDatabaseUser(ctx context.Context, database string, username string, password string) error
 	DeleteDatabaseUser(ctx context.Context, database string, username string) error
 	Close(ctx context.Context) error
 }
